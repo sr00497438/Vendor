c73b04ad6024c32848d7e1b840736a8b0a760393
TransportTechm <36197874+TransportTechm@users.noreply.github.com>
2018-02-16 19:11:29 +0530

    Update MyApplication.java

--- src/main/java/com/techm/MyApplication.java
+++ src/main/java/com/techm/MyApplication.java
@@ -6,7 +6,7 @@ public class MyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 		
-		//Hello None
+		//Hello Sarath
 			
     }       
 }            

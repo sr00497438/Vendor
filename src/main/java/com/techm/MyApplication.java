03e33cde2cd85559943c4137566287e93130a9e5
TransportTechm <36197874+TransportTechm@users.noreply.github.com>
2018-02-16 19:09:02 +0530

    Update MyApplication.java

--- src/main/java/com/techm/MyApplication.java
+++ src/main/java/com/techm/MyApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {  
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
-		//Hello None
 		
-		
+		//Heello None
+			
     }       
 }            

273c30be8f8355e74f652e6eb7e34494b2aa3070
sarath <sarathchandravarma.k@gmail.com>
2018-02-22 11:11:05 +0530

    Latest Push

--- src/main/java/com/techm/transport/vendor/VendorApplication.java
+++ src/main/java/com/techm/transport/vendor/VendorApplication.java
@@ -1,7 +1,13 @@
 package com.techm.transport.vendor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication

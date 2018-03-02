325573c65035f42b56e7ee3d48635e8b8b4572be
TransportTechm <36197874+TransportTechm@users.noreply.github.com>
2018-03-02 14:13:58 +0530

    Update DriverController.java

--- src/main/java/com/techm/transport/vendor/controller/DriverController.java
+++ src/main/java/com/techm/transport/vendor/controller/DriverController.java
@@ -25,7 +25,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 @RequestMapping("1.0")
 @Api(description="Driver operations", tags= {"Drivers"})

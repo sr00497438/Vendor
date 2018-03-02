727366476ce49379ad3c4e91fa9cdf40394f4605
TransportTechm <36197874+TransportTechm@users.noreply.github.com>
2018-03-02 14:14:58 +0530

    Update VehicleTypeController.java

--- src/main/java/com/techm/transport/vendor/controller/VehicleTypeController.java
+++ src/main/java/com/techm/transport/vendor/controller/VehicleTypeController.java
@@ -24,7 +24,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 @RequestMapping("1.0")
 @Api(description="VehicleType operations", tags= {"Vehicle Types"})

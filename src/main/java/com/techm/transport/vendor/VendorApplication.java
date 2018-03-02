88611e3eadcc328a5c652e42e6f92017320642cf
TransportTechm <36197874+TransportTechm@users.noreply.github.com>
2018-03-02 14:26:49 +0530

    Update VendorApplication.java

--- src/main/java/com/techm/transport/vendor/VendorApplication.java
+++ src/main/java/com/techm/transport/vendor/VendorApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+@CrossOrigin
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
